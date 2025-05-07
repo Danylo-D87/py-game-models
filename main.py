720e46c94f86fbf6bdf23ded80fab37156837e87
Danylo-D87 <quaestor88@gmail.com>
2025-05-07 16:50:38 +0300

    sol 2

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main() -> None:
         if guild_data:
             guild, _ = Guild.objects.get_or_create(
                 name=guild_data["name"],
-                description=guild_data.get("description", None)
+                description=guild_data.get("description", "")
             )
         else:
             guild = None
